950b3e6a9b584c9258dd13f5d4ce06561aa952ed
Jorge Morales <luifer991@gmail.com>
2024-11-06 11:39:03 -0500

    first

---
+++ EMployeeAdminPortal/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using EMployeeAdminPortal.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EMployeeAdminPortal.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        //{ hacer el contructor manualmente
+
+        //}
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ EMployeeAdminPortal/Migrations/20241104192217_second.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EMployeeAdminPortal.Migrations
+{
+    /// <inheritdoc />
+    public partial class second : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Employees",
+                newName: "Email");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "Employees",
+                newName: "Description");
+        }
+    }
+}
